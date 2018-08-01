092406e7429940b2f40cfc2cb3fb0cf034ce6631
Anton Pozivenec <anton.pozivenec@escapemotions.com>
2018-08-01 12:37:26 +0200

    disabled creating of new panel on Undock

---
+++ panelholder.cpp
@@ -0,0 +1,6 @@
+#include "panelholder.h"
+
+PanelHolder::PanelHolder()
+{
+	
+}
---
+++ panelholder.h
@@ -0,0 +1,12 @@
+#ifndef PANELHOLDER_H
+#define PANELHOLDER_H
+
+#include <QObject>
+
+class PanelHolder
+{
+	public:
+		PanelHolder();
+};
+
+#endif // PANELHOLDER_H
